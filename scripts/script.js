4226ede9c0bb579c6ff4c0b3acb3fe2d8aa778c5
patel1643 <42159154+patel1643@users.noreply.github.com>
2020-02-01 19:02:45 -0500

    final

--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,20 @@ $(document).ready(function () {
               $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
           }
       });
+
+
+
+
+// //
 // $(window).scroll(function(){
-//
-// 	$('.navbar-default.scrolled').toggleClass('.navbar-default', $(this).scrollTop() > 2730));
-//   $('.a-bar.scrolled').toggleClass('.a-bar', $(this).scrollTop() > 2730));
-// 	$('.a.scrolled').toggleClass('.a', $(this).scrollTop() > 2730));
+// 	$('nav').toggleClass('scrolled', $(this).scrollTop() > 730);
+//   $('.a').toggleClass('scrolled', $(this).scrollTop() >730);
+// 	$('.a-bar').toggleClass('scrolled', $(this).scrollTop() > 730);
 // });
-// window.onscroll = () => {
-//    const nav = document.querySelector('.navbar');
-//   if(this.scrollY <= 730) {nav.className = '';}
-//   else if(this.scrollY >= 730   and  this.scrollY <= 7300){ nav.className = 'scrolled';}
-//   else nav.className = '';
-// };
+//
+// // ScrollBar Custom
+// $(document).ready(function () {
+//           if (!$.browser.webkit) {
+//               $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
+//           }
+//       });
