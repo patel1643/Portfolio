120cd9ac19e72b0a57cac5852321599deb7180ce
patel1643 <42159154+patel1643@users.noreply.github.com>
2020-12-01 23:30:04 -0500

    fixed the background

--- scripts/script.js
+++ scripts/script.js
@@ -1,24 +1,16 @@
 $(window).scroll(function(){
-	$('nav').toggleClass('scrolled', $(this).scrollTop() > 730);
-  $('.a').toggleClass('scrolled', $(this).scrollTop() >730);
-	$('nav').toggleClass('navbar-dark',  $(this).scrollTop() < 730);
+	$('nav').toggleClass('scrolled', $(this).scrollTop() > 600);
+  $('.a').toggleClass('scrolled', $(this).scrollTop() >600);
+	$('nav').toggleClass('navbar-dark',  $(this).scrollTop() < 600);
 
 });
 //This function changes the hamburger colour when bg is white to
 $(window).scroll(function(){
-	$('nav').addClass('navbar-light', $(this).scrollTop() >730);
+	$('nav').addClass('navbar-light', $(this).scrollTop() >600);
 });
 
 // ScrollBar Custom
 $(document).ready(function () {
           if (!$.browser.webkit) {
               $('.wrapper').html('<p>Sorry! Non webkit users. :(</p>');
-          }
-      });
-//
-// $(window).scroll(function(){
-// 	$('nav').toggleClass('scrolled', $(this).scrollTop() < 730);
-//   $('.a').toggleClass('scrolled', $(this).scrollTop() >730);
-// 	$('nav').toggleClass('navbar-dark',  $(this).scrollTop() < 730);
-//
-// });
+          }});
